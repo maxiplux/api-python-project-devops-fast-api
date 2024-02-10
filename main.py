da69529db880b11815f424e1757341f5da8096dc
Juan Francisco Mosquera <maxiplux@gmail.com>
2024-02-09 20:43:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from app.routers.v1 import items
 from app.routers import auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
